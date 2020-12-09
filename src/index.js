62aae185faaa20a866870129619a938ecba1bc99
Niko <lukaggt2003@gmail.com>
2020-12-09 13:21:02 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '/Users/luka/webgifaby/src/Ladding.css'
+import './Ladding.css'
 
 ReactDOM.render(
   <React.StrictMode>
